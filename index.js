96b76cfc8452d23b5d9126e93655a4928bb293fe
Osama Ahmed <osamaahmedwork1@gmail.com>
2025-07-14 21:43:46 +0300

    orign changes

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(
   cors({
-    origin: ['http://localhost:5173'],
+    origin: 'http://localhost:5173',
     credentials: true,
     allowedHeaders: ['Content-Type', 'Authorization'],
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
